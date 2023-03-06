16625a5481c743ca0d2fea542c7ed3a07bf57e6a
davidbajnai <david.bajnai@gmail.com>
2023-03-06 17:07:53 +0100

    after coffee

--- uploadSequence.php
+++ uploadSequence.php
@@ -3,20 +3,21 @@
     // This script is used to:
     // load a sequence .csv file
 
-    /* Get the name of the uploaded file */
+    // Get the name of the uploaded file
     $filename = $_FILES['file']['name'];
 
-    /* Choose where to save the uploaded file */
+    // Choose where to save the uploaded file
     $location = "./Sequences/" . $filename;
 
-    /* Save the uploaded file to the local filesystem */
-    if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
-        // echo 'Success'; 
-    }
-    else
-    { 
-        // echo 'Failure'; 
-    }
+    // Save the uploaded file to the local filesystem
+    move_uploaded_file($_FILES['file']['tmp_name'], $location);
+    // if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
+    //     // echo 'Success'; 
+    // }
+    // else
+    // { 
+    //     // echo 'Failure'; 
+    // }
 
     $file = fopen($location, "r");
     $col1_array = [];
