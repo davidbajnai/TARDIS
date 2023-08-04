43e1c813aeb6944799a64f756ee58c3829b1787e
davidbajnai <david.bajnai@gmail.com>
2023-08-04 13:00:44 +0200

    after coffee

--- sendCommand.php
+++ sendCommand.php
@@ -13,6 +13,18 @@ if( $_POST['cmd'] != "" )
 {
     if( $_POST['cmd'] == "TWD1" )
     {
+        // Send command via TCP
+        // $socket = fsockopen('192.168.1.240', 12345, $errno, $errstr, 10);
+        // if (!$socket) {
+        //     // Handle connection error
+        //     echo "Error: $errno - $errstr";
+        // } else {
+        //     // Send the message: "WD on" and "ASS on"
+        //     fwrite($socket, "amass1\r\namwd1\r\n");
+        //     // Close the connection
+        //     fclose($socket);
+        // }
+
         // get last modified date of the commands logfile on the TILDAS PC
         $oldFileTime = exec( "date -r /mnt/TILDAS-CS-132/Commands/comlog.dat" );
 
@@ -32,6 +44,18 @@ if( $_POST['cmd'] != "" )
     }
     else if( $_POST['cmd'] == "TWD0" )
     {
+        // Send command via TCP
+        // $socket = fsockopen('192.168.1.240', 12345, $errno, $errstr, 10);
+        // if (!$socket) {
+        //     // Handle connection error
+        //     echo "Error: $errno - $errstr";
+        // } else {
+        //     // Send the message: "WD off" and "ASS off"
+        //     fwrite($socket, "amass0\r\namwd0\r\n");
+        //     // Close the connection
+        //     fclose($socket);
+        // }
+
         // get last modified date of the commands logfile on the TILDAS PC
         $oldFileTime = exec( "date -r /mnt/TILDAS-CS-132/Commands/comlog.dat" );
 
@@ -50,9 +74,9 @@ if( $_POST['cmd'] != "" )
         }
     }
     else
-    {    
-        $m->set('key2', $_POST['cmd']); // This is a command sent to Python serial communication
-        // echo("Command sent to Arduino");
+    {
+        // This is a command sent to Python serial communication
+        $m->set('key2', $_POST['cmd']);
     }
 }
 // clean up
